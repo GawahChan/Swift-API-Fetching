150e75ff30582d534d5123e8bf88fc0e53a71f79
Gawah Chan <gawahchan@gmail.com>
2023-08-15 18:49:25 +0100

    Initial Commit

---
+++ Swift-API-Fetching/Swift_API_FetchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_API_FetchingApp.swift
+//  Swift-API-Fetching
+//
+//  Created by Gawah Chan on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_API_FetchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
