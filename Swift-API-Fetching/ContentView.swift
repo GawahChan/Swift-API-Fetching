580ef40e6fcb764cbc8d8ff1949180632347950f
Gawah Chan <gawahchan@gmail.com>
2023-08-17 10:39:48 +0100

    Fetch Taylor swift songs

--- Swift-API-Fetching/ContentView.swift
+++ Swift-API-Fetching/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(results, id: \.trackId) { track in
+                VStack {
+                    Text(track.trackName)
+                }
+            }
+            .navigationTitle("Taylor Swift Songs")
+            .task {
+                await fetchData()
+            }
         }
-        .padding()
+    }
+    
+    func fetchData() async {
+        print("fetching data...")
+        
+        // create url
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        // fetch data from url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("API not working")
+        }
     }
 }
 
---
+++ Swift-API-Fetching/Models/DadJoke.swift
@@ -0,0 +1,10 @@
+//
+//  DadJoke.swift
+//  Swift-API-Fetching
+//
+//  Created by Gawah Chan on 15/08/2023.
+//
+
+import Foundation
+
+
